6b2cf33687f38512518133d54c6f25ef41a088b8
Timo Kilb <timo@swinx.app>
2020-12-02 18:23:47 +0100

    convenience functions for vertex.js and graph.js

--- src/Model/Graph.js
+++ src/Model/Graph.js
@@ -1,9 +1,52 @@
 class Graph {
 
     constructor() {
-        self.vertices = []
-        self.edges = []
+        this.vertices = []
+        this.edges = []
+        this.hasVertices = this.hasVertices.bind(this)
+        this.getVertices = this.getVertices.bind(this)
+        this.getVertexAtIndex = this.getVertexAtIndex.bind(this)
+        this.hasEdges = this.hasEdges.bind(this)
+        this.getEdges = this.getEdges.bind(this)
+        this.sortVerticesByXPos = this.sortVerticesByXPos.bind(this)
+        this.sortVerticesByYPos = this.sortVerticesByYPos.bind(this)
     }
 
+    hasVertices(){
+        return (this.vertices.length < 0) ? true : false
+    }
+
+    // Also for getEdges, is this the right way to handle an undefined error?
+    // Do we even need to write a getter since you could just access the Instances proterty
+    getVertices(){
+        if (!this.hasVertices()){
+            throw "Graph has no edges"
+        }
+            return this.vertices
+    }
+
+    getVertexAtIndex(index){
+        return this.vertices[index]
+    }
+
+    sortVerticesByXPos(){
+        //TODO: Implement error handling if
+        return this.vertices.sort((x, y) => x.xPos - y.xPos)
+    }
+
+    sortVerticesByYPos(){
+        return this.vertices.sort((x, y) => x.yPos - y.yPos)
+    }
+
+    hasEdges(){
+        return (this.edges.length < 0) ? true : false
+    }
+    
+    getEdges(){
+        if (!this.hasEdges()){
+            throw "Graph has no edges"
+        }
+            return this.edges
+    }
 
 }
--- src/Model/Vertex.js
+++ src/Model/Vertex.js
@@ -5,7 +5,11 @@ class Vertex {
         this.xPos = xPos
         this.yPos = yPos
         this.incidentEdges = []
-
+        this.getXPos = this.getXPos.bind(this)
+        this.getYPos = this.getYPos.bind(this)
+        this.getIncidentEdges = this.getIncidentEdges.bind(this)
+        this.hasIncidentEdges = this.hasIncidentEdges.bind(this)
+        this.removeAllEdges = this.removeAllEdges.bind(this)
     }
 
     getXPos(){
@@ -24,5 +28,16 @@ class Vertex {
         return this.incidentEdges.length == 0 ? false : true
     }
 
+    // Check if throwing a string is the right thing to do here
+    randomIncidentEdge(){
+        if (!this.hasIncidentEdges()){
+            throw "Vertex has no incident edges"
+        }
+            return this.incidentEdges[Math.floor(Math.random()*items.length)]
+    }
+
+    removeAllEdges() {
+        this.incidentEdges = []
+    }
 
 }
