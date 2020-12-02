8c92a1f642cb9de91690637df304f57d2f39fa27
nagod <deniz-dogan@t-online.de>
2020-12-02 19:13:47 +0100

    modified Error in src/Model/Vertex

--- src/Model/Vertex.js
+++ src/Model/Vertex.js
@@ -1,43 +1,44 @@
 class Vertex {
+  constructor(xPos, yPos) {
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.incidentEdges = [];
+    this.getXPos = this.getXPos.bind(this);
+    this.getYPos = this.getYPos.bind(this);
+    this.getIncidentEdges = this.getIncidentEdges.bind(this);
+    this.hasIncidentEdges = this.hasIncidentEdges.bind(this);
+    this.removeAllEdges = this.removeAllEdges.bind(this);
+  }
 
-    constructor(xPos, yPos){
-        
-        this.xPos = xPos
-        this.yPos = yPos
-        this.incidentEdges = []
-        this.getXPos = this.getXPos.bind(this)
-        this.getYPos = this.getYPos.bind(this)
-        this.getIncidentEdges = this.getIncidentEdges.bind(this)
-        this.hasIncidentEdges = this.hasIncidentEdges.bind(this)
-        this.removeAllEdges = this.removeAllEdges.bind(this)
+  getXPos() {
+    return this.xPos;
+  }
+
+  getYPos() {
+    return this.yPos;
+  }
+
+  getIncidentEdges() {
+    return this.incidentEdges;
+  }
+
+  hasIncidentEdges() {
+    return this.incidentEdges.length == 0 ? false : true;
+  }
+
+  // Check if throwing a string is the right thing to do here
+  randomIncidentEdge() {
+    if (!this.hasIncidentEdges()) {
+      throw new Error({
+        message: "Vertex has no incident edges",
+      });
     }
+    return this.incidentEdges[
+      Math.floor(Math.random() * this.incidentEdges.length)
+    ];
+  }
 
-    getXPos(){
-        return this.xPos
-    }
-
-    getYPos(){
-        return this.yPos
-    }
-
-    getIncidentEdges(){
-        return this.incidentEdges
-    }
-
-    hasIncidentEdges(){
-        return this.incidentEdges.length == 0 ? false : true
-    }
-
-    // Check if throwing a string is the right thing to do here
-    randomIncidentEdge(){
-        if (!this.hasIncidentEdges()){
-            throw "Vertex has no incident edges"
-        }
-            return this.incidentEdges[Math.floor(Math.random()*items.length)]
-    }
-
-    removeAllEdges() {
-        this.incidentEdges = []
-    }
-
+  removeAllEdges() {
+    this.incidentEdges = [];
+  }
 }
