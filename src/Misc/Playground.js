a0d2cf24158606adad607e0deeeebc61b3841de2
nagod <deniz-dogan@t-online.de>
2021-03-17 11:26:19 +0100

    l

--- src/Misc/Playground.js
+++ src/Misc/Playground.js
@@ -1,19 +1,5 @@
-function union(arr1, arr2) {
-    //null check
-    if (!arr1.includes(Infinity) && !arr2.includes(Infinity)) {
-        if (arr1.length < arr2.length) {
-            // add elements from arr1 to arr2
-            arr1.forEach(element => {
-                arr2.push(element)
-                arr1.pop()
-            })
-            arr1[0] = Infinity
-        } else {
-            arr2.forEach(element => {
-                arr1.push(element)
-                arr2.pop()
-            })
-            arr2[0] = Infinity
-        }
-    }
-}
+let arr = [1, 2, 3, 4, 5]
+console.log(arr)
+
+arr = []
+console.log(arr.length)
